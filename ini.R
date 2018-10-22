ee5223aa32658e5079c17d3d985808d202cb68e5
kiwimic <michalsiwik@gmail.com>
2018-10-22 20:49:41 +0200

    Nie działa w domu (trzeba zobaczyć dlaczego)

--- ini.R
+++ ini.R
@@ -1,3 +1,3 @@
 library(shiny)
 
-shiny::runApp(appDir = getwd(), host = "192.168.42.68", port = 8081, launch.browser = T)
+shiny::runApp(appDir = getwd(), host = "192.168.42.68", port = 8080, launch.browser = T)
